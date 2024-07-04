735c22abde317d6eaa0425462d66c19fb72cb4f1
AbhishekKarn <Abhishek1399@users.noreply.github.com>
2024-07-04 17:22:22 +0530

    Initial commit

---
+++ src/test/java/abhisheklearning/AllTests.java
@@ -0,0 +1,170 @@
+package abhisheklearning;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class AllTests {
+
+
+
+    @Test()
+    public void Test_Scenario1() {
+        WebDriver driver = new ChromeDriver();
+        String givenText = "Welcome to LambdaTest";
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://www.lambdatest.com/selenium-playground/");
+        driver.findElement(By.xpath("//a[contains(text(),'Simple Form Demo')]")).click();
+        String validateUrlName = driver.getCurrentUrl().split("playground")[1].split("/")[1];
+        Assert.assertEquals("simple-form-demo", validateUrlName);
+
+        driver.findElement(By.xpath("//input[@id='user-message']")).sendKeys(givenText);
+        driver.findElement(By.xpath("//button[@id='showInput']")).click();
+
+        String result = driver.findElement(By.xpath("//p[@id='message']")).getText();
+        Assert.assertEquals(givenText, result);
+        System.out.println("Code works fine");
+        driver.quit();
+
+    }
+
+    @Test()
+    public void Test_Scenario2() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://www.lambdatest.com/selenium-playground/");
+
+        try {
+            WebElement dragAndDropSlidersLink = driver.findElement(By.linkText("Drag & Drop Sliders"));
+            dragAndDropSlidersLink.click();
+
+            // Select the slider with default value 15
+            WebElement slider = driver.findElement(By.xpath("//input[@value='15']"));
+            WebElement sliderValueDisplay = driver.findElement(By.id("rangeSuccess")); // Assuming the range value display has id "rangeSuccess"
+
+            // Initialize Actions class
+            Actions actions = new Actions(driver);
+
+            // Calculate the offset required to move the slider from 15 to 95
+            int offset = calculateOffset(slider, 95);
+
+            // Drag the slider
+            actions.dragAndDropBy(slider, offset, 0).perform();
+
+            // Validate whether the range value shows 95
+            String rangeValue = sliderValueDisplay.getText();
+            if ("95".equals(rangeValue)) {
+                System.out.println("Slider successfully moved to 95. Test Passed.");
+            } else {
+                System.out.println("Slider value is " + rangeValue + ". Test Failed.");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+     driver.quit();
+    }
+
+    /**
+     * Calculate the offset required to move the slider from its current value to the target value.
+     */
+
+    private static int calculateOffset(WebElement slider, int targetValue) {
+        // Assuming the slider's range is from 0 to 100
+        int currentValue = Integer.parseInt(slider.getAttribute("value"));
+        int sliderWidth = slider.getSize().getWidth();
+
+        // Calculate the offset
+        int offset = (int) ((targetValue - currentValue) / 100.0 * sliderWidth);
+        return offset;
+    }
+
+
+
+    @Test()
+    public void Test_Scenario3() throws InterruptedException
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.lambdatest.com/selenium-playground/input-form-demo");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement submit = driver.findElement(By.cssSelector("#seleniumform > div.text-right.mt-20 > button"));
+        submit.click();
+       WebElement Name = driver.findElement(By.id("name"));
+        String pleasefilloutthisform = Name.getAttribute("required");
+        Assert.assertTrue(true, pleasefilloutthisform);
+        String errormsg = "Please fill out this field.";
+        Assert.assertTrue(true, errormsg);
+        WebElement country = driver.findElement(By.xpath("//*[@id='seleniumform']/div[3]/div[1]/select"));
+       country.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Wait for the options to be visible
+        WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("country")));
+
+        // Click the drop-down to show options
+        dropdown.click();
+
+        // Locate the options within the drop-down menu
+        List<WebElement> options = driver.findElements(By.xpath("//select[@name='country']/option"));
+
+        // Iterate through the options and select "United States"
+        for (WebElement option : options) {
+            if (option.getText().equals("United States")) {
+                option.click();
+                break;
+            }
+        }
+
+
+        WebElement name = driver.findElement(By.xpath("//*[@id='name']"));
+        name.sendKeys("Test Lambda");
+
+        WebElement Email = driver.findElement(By.cssSelector("#inputEmail4"));
+        Email.sendKeys("TestDataCheck@gmail.com");
+
+        WebElement password = driver.findElement(By.id("inputPassword4"));
+        password.sendKeys("Test$Lamda#@123");
+
+        WebElement Company = driver.findElement(By.xpath("//*[@id='company']"));
+        Company.sendKeys("Test_Lamda pvt ltd.");
+
+        WebElement Website  = driver.findElement(By.cssSelector("#websitename"));
+        Website.sendKeys("www.Test_Lamda.com");
+
+        WebElement City = driver.findElement(By.id("inputCity"));
+        City.sendKeys("Earth");
+
+        WebElement Address1 = driver.findElement(By.xpath("//*[@id='inputAddress1']"));
+       Address1.sendKeys("Earth_Air");
+
+        WebElement Address2 = driver.findElement(By.cssSelector("#inputAddress2"));
+       Address2.sendKeys("Earth Water");
+
+        WebElement state = driver.findElement(By.id("inputState"));
+        state.sendKeys("Test_Lamda State");
+
+        WebElement Zipcode =  driver.findElement(By.xpath("//*[@id='inputZip']"));
+        Zipcode.sendKeys("111111");
+
+
+        submit.click();
+       String submit_msg = "Thanks for contacting us, we will get back to you shortly.";
+        String actual_submit = driver.findElement(By.xpath("//p[@class='success-msg hidden']")).getText().trim();
+       Assert.assertEquals(actual_submit, submit_msg);
+       driver.quit();
+}
+}
+
